1b6cd29ce7621ba468d6d6723c0e7e327e564157
happyuniv <scv0930@daum.net>
2023-02-13 22:02:32 +0900

    Add. favicon & head tag

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <meta httpEquiv='content-language' content='ko' />
+      <meta name='description' content='프론트엔드 학습 문서' />
+      <meta property='og:description' content='프론트엔드 학습 문서' />
+      <meta property='og:title' content='Happyuniv Docs' />
+      <link rel='icon' href='/favicon.ico' />
+      <title>Happyuniv Docs</title>
+    </>
+  ),
   logo: (
     <>
       <img src='https://avatars.githubusercontent.com/u/97126757?v=4' />
